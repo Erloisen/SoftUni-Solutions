4c7c370e9d6e404bb84c882fea1619490e6abb8a
Elisaveta Barzanova <84933621+Erloisen@users.noreply.github.com>
2021-10-30 16:06:53 +0300

    Solutions for Encapsulation lab problems.

---
+++ CSharp-OOP/Encapsulation-Lab/SortPersonsByNameAndAge/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+            Team team = new Team("SoftUni");
+            for (int i = 0; i < lines; i++)
+            {
+                try
+                {
+                    string[] inputInfo = Console.ReadLine().Split();
+                    var person = new Person(inputInfo[0], inputInfo[1], int.Parse(inputInfo[2]), decimal.Parse(inputInfo[3]));
+                    persons.Add(person);
+                }
+                catch (Exception m)
+                {
+
+                    Console.WriteLine(m);
+                }
+            }
+
+            foreach (Person currentPerson in persons)
+            {
+                team.AddPlayer(currentPerson);
+            }
+
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
+        }
+    }
+}
